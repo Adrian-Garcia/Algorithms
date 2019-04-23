0034f0f28556b76d48e0184fcbd2c6750e1e8d55
Adrian Garcia <adriangarlop@hotmail.com>
2019-04-23 14:12:32 -0500

    Eddited Debuger

--- Debuger.cpp
+++ Debuger.cpp
@@ -1,30 +1,4 @@
 #include <iostream>
-<<<<<<< HEAD
-using namespace std;
-
-int CN(int n){
-  int Cn =0;
-  // base case
-  if(n==0) // empty tree
-  {
-    return 1;
-  }
-  for(int i=1;i<n+1;i++)
-  {
-    Cn+= CN(i-1)*CN(n-i);
-  }
-  return Cn;
-}
-
-int main(){
-  int n = 19;
-  cout<<"Enter number of nodes: ";
-//  cin>>n;
-  cout<<n<<endl;
-  int trees=CN(n);
-  cout<<trees<<" binary search trees are there for "<<n<<" nodes"<<endl;
-  return 0; 
-=======
 #include <vector>
 using namespace std;
 
@@ -40,7 +14,6 @@ unsigned long int numTrees(int n){
     }
   }
   return D[n];
->>>>>>> dev
 }
 
 int main() {
