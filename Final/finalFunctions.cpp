e81ae66b474634442af73e3b073200d83fd9990f
Adrian Garcia <adriangarlop@hotmail.com>
2019-05-19 08:44:12 -0500

    Added railRoad function at finalFunctions.cpp

--- Final/finalFunctions.cpp
+++ Final/finalFunctions.cpp
@@ -168,6 +168,50 @@ double median(double arr1[], double arr2[], int n) {
 	}	
 }
 
+bool railRoad(int N1, int N2, int train1[], int train2[], int order[]) {
+	
+	bool flag[N1+1][N2+1];					// To validate
+	int i, j;								// Indexes
+
+	flag[0][0] = true;
+
+	for (i=0; i<N1; i++) {
+		
+		if (flag[i][0])
+			flag[i+1][0] = (order[i+1]==train1[i+1]) ? true : false;
+
+		else 
+			flag[i+1][0] = false;
+	}
+
+	for (i=0; i<N2; i++) {
+		
+		if (flag[0][i]) 
+			flag[0][i+1] = (order[i+1]==train2[i+1]) ? true : false;	
+
+		else 
+			flag[0][i+1] = false;
+	}
+
+	for (i=1; i<=N1; i++) {
+		
+		for (j=1; j<=N2; j++) {
+			
+			if (order[i+j] == train1[i] && flag[i-1][j])
+				flag[i][j] = true;
+			
+			else if (order[i+j] == train2[j] && flag[i][j-1])
+				flag[i][j] = true;
+
+			else
+				flag[i][j] = false;
+
+		}
+	}
+	
+	return flag[N1][N2];
+}
+
 int main() {
 
 	return 0;
